a85838606657f0be18525659efa924b1f70f152f
DavidV1984 <david.vegelj@gmail.com>
2019-07-30 21:34:48 +0200

    Dodani komentarji v slovenščini

--- central_bank_rates.py
+++ central_bank_rates.py
@@ -5,50 +5,57 @@ import pandas as pd
 import datetime as dt
 import numpy as np
 
-# nastavimo pogled za pandas
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-
 
 # na html kodi poišèemo vse tabele in izlušèimo le tisto, ki jo potrebujemo [9]. Nastavimo indeks.
 df = pd.read_html('https://www.global-rates.com/interest-rates/central-banks/central-banks.aspx', header=0)[9].set_index('country/region')
+
+# odstranimo dva stolpca
 df.drop(['direction', 'Name of interest rate'], axis=1, inplace=True)
 
+# popravimo trenutno in preteklo stopnjo
 df['current rate'] = [x.strip().replace(u'\xa0%', u'') for x in df['current rate']]
 df['current rate'] = pd.to_numeric(df['current rate'])
-
 df['previous rate'] = [x.strip().replace(u'\xa0%', u'') for x in df['previous rate']]
 df['previous rate'] = pd.to_numeric(df['previous rate'])
 
+# spremenimo v datetime
 df['change'] = pd.to_datetime(df['change'])
 
+# izraèunamo razliko v dnevih in nastavimo trend
 df['razlika'] = dt.datetime.today() - df['change']
 df['OMt'] = np.where(df['razlika'] > dt.timedelta(183), 0, np.where(df['current rate'] - df['previous rate'] > 0, -1, +1))
 
-df = df[['current rate', 'OMt']]
+# uporabimo samo trnutno stopnjo in trend
+df = df[{'current rate', 'OMt'}]
 
+# nastavimo ECB rate, ki ga uporabimo za vse evropske države
 ECB_current_rate = df.at['Europe', 'current rate']
 ECB_OMt = df.at['Europe', 'OMt']
 #print(ECB_current_rate, ECB_OMt)
 
-
+# pripravimo nov indeks z državami, ki nas zanimajo
 new_index = ['United States', 'Germany', 'Japan', 'China', 'India', 'Russia',
              'Brazil', 'Slovenia', 'Turkey', 'Mexico', 'Indonesia', 'Poland',
              'Italy', 'France', 'Australia', 'Spain', 'South Korea', 'Great Britain']
 
+# reindeksiramo z novim indeksom
 df = df.reindex(new_index)
 
+# za evropske države doloèimo obrestno mero
 list = ['Germany', 'Slovenia', 'Italy', 'France', 'Spain']
 
 for i in list:
     df.at[i, 'current rate'] = ECB_current_rate
     df.at[i, 'OMt'] = ECB_OMt
 
+# spremenimo ime indeksa in stolpca
 df.index.names = ['Država']
 df.rename(columns={'current rate': 'OM'}, inplace=True)
 
+# resetiramo indeks
 df.reset_index(inplace=True)
 
+# preimenujemo države v slovenšèino
 preimenovanje = [(df['Država'] == 'United States'), (df['Država'] == 'Japan'), (df['Država'] == 'Slovenia'),
                  (df['Država'] == 'Germany'), (df['Država'] == 'Turkey'), (df['Država'] == 'Mexico'),
                  (df['Država'] == 'India'), (df['Država'] == 'Indonesia'), (df['Država'] == 'Italy'),
